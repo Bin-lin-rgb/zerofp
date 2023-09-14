3b38da8a79ecabe824da4948e5abace555ac7a65
Bin-lin-rgb <3036140937@qq.com>
2023-09-14 23:04:53 +0800

    feat user gen

--- define/define.go
+++ define/define.go
@@ -1,7 +1,23 @@
 package define
 
-import "os"
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"os"
+)
 
 var (
-	MysqlDSN = os.Getenv("mysqlDsn")
+	MysqlDSN   = os.Getenv("mysqlDsn")
+	EmqxKey    = "1f9c5b734fe27865"
+	EmqxSecret = "lV9C2iefOp9Cr9BeiB5rr3N9CBolJjKk3HruhqEpHQxsuVD"
+)
+
+type UserClaim struct {
+	Id       uint   `json:"id"`
+	Identity string `json:"identity"`
+	Name     string `json:"name"`
+	jwt.RegisteredClaims
+}
+
+var (
+	JwtKey = "iot-platform"
 )
---
+++ user/internal/logic/user_login_logic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"zerofp/user/internal/svc"
+	"zerofp/user/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserLoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLoginLogic {
+	return &UserLoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserLoginLogic) UserLogin(req *types.UserLoginRequest) (resp *types.UserLoginReply, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
